2eb1f47ca220c09e7707ecb004ef549fbac122d5
domenico2003 <domdat03@gmail.com>
2023-05-23 16:14:55 +0200

    aggiunta metodo bigliettiVendutiPerTempo

--- src/dao/TitoliDiViaggioDAO.java
+++ src/dao/TitoliDiViaggioDAO.java
@@ -1,10 +1,11 @@
 package dao;
 
 import java.time.LocalDate;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
-
 import javax.persistence.Query;
 
 import entities.Abbonamento;
@@ -18,38 +19,52 @@ public class TitoliDiViaggioDAO {
 	public TitoliDiViaggioDAO(EntityManagerFactory emf) {
 		this.emf = emf;
 	}
-	
+
 	public void saveBiglietto(Biglietto b) {
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction transaction = em.getTransaction();
 		transaction.begin();
 		em.persist(b);
-		transaction.commit(); 
+		transaction.commit();
 		System.out.println("Titolo di viaggio creato!");
 		em.close();
 	}
-	
+
+	public List<Biglietto> bigliettiVendutiPerTempo(LocalDate da, LocalDate a) {
+		EntityManager em = emf.createEntityManager();
+		EntityTransaction transaction = em.getTransaction();
+		transaction.begin();
+		Query q = em.createQuery("SELECT b FROM Biglietto b WHERE b.dataEmissione>= :from AND  b.dataEmissione<= :to");
+		q.setParameter("from", da);
+		q.setParameter("to", a);
+
+		List<Biglietto> results = q.getResultList();
+		transaction.commit();
+		em.close();
+		return results;
+	}
+
 	public void saveAbbonamento(Abbonamento a) {
 		EntityManager em = emf.createEntityManager();
 		EntityTransaction transaction = em.getTransaction();
 		transaction.begin();
 		em.merge(a);
-		transaction.commit(); 
+		transaction.commit();
 		System.out.println("Titolo di viaggio creato!");
 		em.close();
 	}
-	
+
 	public boolean checkValiditaAbbonamento(Tessera numeroTessera) {
 		EntityManager em = emf.createEntityManager();
 		Query q = em.createQuery("SELECT a FROM Abbonamento a WHERE a.numeroTessera = :numeroTessera");
-		
+
 		q.setParameter("numeroTessera", numeroTessera);
-		
-		Abbonamento foundAbbonamento = (Abbonamento) q.getSingleResult();	
-		
+
+		Abbonamento foundAbbonamento = (Abbonamento) q.getSingleResult();
+
 		em.close();
-		
-		if(foundAbbonamento.getDataScadenza().isBefore(LocalDate.now()) ) {
+
+		if (foundAbbonamento.getDataScadenza().isBefore(LocalDate.now())) {
 			System.out.println("ABBONAMENTO VALIDO");
 			return true;
 		} else {
@@ -57,5 +72,5 @@ public class TitoliDiViaggioDAO {
 			return false;
 		}
 	}
-	
+
 }
