2b086ef67644dc087f2eb84bfda9398c546b11a1
Luqman Mohammad <123764610+luqi2000@users.noreply.github.com>
2023-05-24 13:37:50 +0100

    some adjustments

--- src/main/Application.java
+++ src/main/Application.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 import javax.persistence.EntityManagerFactory;
 
+import dao.DistributoriAutomaticiDAO;
 import dao.ParcoMezziDAO;
 import dao.PuntiVenditaDAO;
 import dao.TesseraDAO;
@@ -13,10 +14,12 @@ import dao.TitoliDiViaggioDAO;
 import dao.UtenteDAO;
 import entities.Abbonamento;
 import entities.Biglietto;
+import entities.DistributoriAutomatici;
 import entities.ParcoMezzi;
 import entities.PeriodoManutenzione;
 import entities.PeriodoServizi;
 import entities.PuntiVendita;
+import entities.Stato;
 import entities.Tessera;
 import entities.Tratta;
 import entities.Utente;
@@ -53,8 +56,7 @@ public class Application {
 		// pvd.getById("1132b40d-f98a-4ce5-860a-34cacf61385e");
 
 		ParcoMezzi mezzoUno = pmd.getMezzo("82be1454-1136-4a2f-b729-c971423fec35");
-		//mezzoUno.getBiglietti().stream().forEach(b -> log.info(b.toString()));
-
+		
 		Biglietto biglietto1 = new Biglietto(LocalDate.of(2023, 05, 22), true, pvendita1, true, mezzoUno,
 				LocalDate.of(2023, 05, 23));
 		// tvd.saveBiglietto(biglietto1);
@@ -76,6 +78,9 @@ public class Application {
 		// System.out.println(mezzoUno.getPeriodoServizio());
 
 		// tvd.checkValiditaAbbonamento(tesseraUno);
+		
+		//mezzoUno.getBiglietti().stream().forEach(b -> log.info(b.toString()));
+
 
 		//pvd.getTicketsBySalesPoint("88db3b5f-e0f3-4341-b933-8d3cdeae2636");
 
@@ -184,5 +189,93 @@ public class Application {
 		int minuti = scan.nextInt();
 
 		return LocalDateTime.of(anno, mese, giorno, ora, minuti);
+	}
+	
+	
+
+	public static void PuntoVendita() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("1. Aggiungi punto vendita");
+		System.out.println("2. Cerca un punto vendita");
+		System.out.println("3. Elimina un punto vendita");
+		System.out.println("4. Visualizza il numero totale dei biglietti e abbonamenti venduti nel punto vendita dato un id");
+		
+		int scelta = scanner.nextInt();
+		
+		scanner.nextLine();
+		
+		EntityManagerFactory emf = JPAUtil.getEntityManagerFactory();
+		PuntiVenditaDAO pvd = new PuntiVenditaDAO(emf);
+		DistributoriAutomaticiDAO da = new DistributoriAutomaticiDAO(emf);
+		
+		
+		switch(scelta) {
+			case 1: 
+				
+				System.out.println("Inserisci true se e' un distributore automatico oppure false se e' un rivenditore autorizzato");
+				boolean distributore = scanner.nextBoolean();
+				if(distributore == false) {
+					
+					System.out.println("Inserisci indirizzo");
+					scanner.nextLine();
+					String indirizzo = scanner.nextLine();
+					
+					System.out.println("Inserisci numeroVendite");
+					Integer numeroVendite = scanner.nextInt();
+					
+					PuntiVendita pvendita2 = new PuntiVendita(indirizzo, numeroVendite, distributore);
+					pvd.save(pvendita2);
+					
+				}else {
+					System.out.println("Inserisci indirizzo");
+					scanner.nextLine();
+					String indirizzo = scanner.nextLine();
+					
+					System.out.println("Inserisci numeroVendite");
+					Integer numeroVendite = scanner.nextInt();
+					
+					System.out.println("Inserisci lo stato del distributore ovvero attivo o disattivo");
+					scanner.nextLine();
+					String statoDistributore = scanner.nextLine();
+					
+					if(statoDistributore.matches("attivo")) {
+						DistributoriAutomatici distributoriAuto = new DistributoriAutomatici(indirizzo, numeroVendite, Stato.attivo);
+						da.save(distributoriAuto);
+						
+					}else if(statoDistributore.matches("disattivo")){
+						DistributoriAutomatici distributoriAuto = new DistributoriAutomatici(indirizzo, numeroVendite, Stato.disattivo);
+						da.save(distributoriAuto);
+					}
+				}
+				
+				break;
+				
+			case 2:
+				System.out.println("Inserisci id");
+				String idpuntoVendita = scanner.nextLine();
+				System.out.println(pvd.getById(idpuntoVendita));
+				break;
+				
+				
+			case 3:
+				System.out.println("Inserisci id");
+				String pvEliminato = scanner.nextLine();
+				pvd.FindAndDelete(pvEliminato);
+				
+				break;
+				
+			case 4:
+				String totBigliettiVenduti = scanner.nextLine();
+				pvd.getTicketsBySalesPoint(totBigliettiVenduti);
+				
+				break;
+				
+			default:
+				System.out.println("Scelta non valida");
+				break;
+		}
+		scanner.close();
+
 	}
 }
