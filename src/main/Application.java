bcaa22cdb7de2f10339bd37cedf654f39ac36ec8
Marianna <121226166+mari-albrici@users.noreply.github.com>
2023-05-24 15:22:07 +0200

    Fixed

--- src/main/Application.java
+++ src/main/Application.java
@@ -19,8 +19,6 @@ import entities.Abbonamento;
 import entities.Biglietto;
 import entities.DistributoriAutomatici;
 import entities.ParcoMezzi;
-import entities.PeriodoManutenzione;
-import entities.PeriodoServizi;
 import entities.PuntiVendita;
 import entities.Stato;
 import entities.Tessera;
@@ -47,119 +45,6 @@ public class Application {
 
 	public static void main(String[] args) {
 
-
-		Utente utente1 = new Utente("Mario", "Rossi");
-		// ud.save(utente1);
-
-		Tessera tessera1 = new Tessera(LocalDate.of(2023, 05, 02));
-		// td.save(tessera1);
-
-		// **** COLLEGAMENTO TESSERA A UTENTE ****
-		// ud.collegaTessera(1, tessera1);
-
-		PuntiVendita pvendita1 = new PuntiVendita("Via Verdi, 18", 1, false);
-		// pvd.save(pvendita1);
-
-		// pvd.getById("1132b40d-f98a-4ce5-860a-34cacf61385e");
-
-		ParcoMezzi mezzoUno = pmd.getMezzo("82be1454-1136-4a2f-b729-c971423fec35");
-		
-		Biglietto biglietto1 = new Biglietto(LocalDate.of(2023, 05, 22), true, pvendita1, true, mezzoUno,
-				LocalDate.of(2023, 05, 23));
-		// tvd.saveBiglietto(biglietto1);
-
-		Tessera tesseraUno = td.getById(1004);
-
-		Abbonamento abbonamento1 = new Abbonamento(LocalDate.of(2023, 03, 10), true, pvendita1, Periodicità.SETTIMANALE,
-				LocalDate.of(2023, 03, 10).plusDays(7), tesseraUno);
-		// tvd.saveAbbonamento(abbonamento1);
-
-		PeriodoManutenzione manutenzione = new PeriodoManutenzione(mezzoUno, LocalDate.of(2022, 12, 01),
-				LocalDate.of(2022, 12, 10));
-		// pmd.savePeriodoManutenzione(manutenzione);
-
-		PeriodoServizi servizio = new PeriodoServizi(mezzoUno, LocalDate.of(2022, 12, 10), LocalDate.of(2023, 12, 01));
-		// pmd.savePeriodoServizio(servizio);
-
-		// System.out.println(mezzoUno.getPeriodoManutenzione());
-		// System.out.println(mezzoUno.getPeriodoServizio());
-
-		// tvd.checkValiditaAbbonamento(tesseraUno);
-		
-		//mezzoUno.getBiglietti().stream().forEach(b -> log.info(b.toString()));
-
-
-		//pvd.getTicketsBySalesPoint("88db3b5f-e0f3-4341-b933-8d3cdeae2636");
-
-		//tvd.bigliettiVendutiPerTempo(LocalDate.of(2023, 05, 10), LocalDate.of(2023, 05, 26)).stream().forEach(b -> log.info(b.toString()));
-
-//		Utente utente1 = new Utente("Mario", "Rossi");
-		//ud.save(utente1);
-	
-//		Tessera tessera1 = new Tessera(LocalDate.of(2023, 05, 02));
-		//td.save(tessera1);
-		
-		// **** COLLEGAMENTO TESSERA A UTENTE ****
-		//ud.collegaTessera(1, tessera1);
-		
-//		Tratta tratta1 = new Tratta("Piazza Dante", "Piazza Leopardi", "45", "15");
-		//pmd.salvaTratta(tratta1);	
-		
-//		Tratta trattaUno = pmd.getTratta("7c6429fe-a518-44d1-9969-523e4f407aa5");
-	
-//		ParcoMezzi mezzo1 = new ParcoMezzi(stato_parcoMezzi.servizio, 75, trattaUno, LocalDateTime.of(2023, 05, 23, 10, 26), LocalDateTime.of(2023, 05, 23, 12, 26), 2, 1, TipoDiMezzo.Autobus);
-		//pmd.salvaMezzo(mezzo1);
-
-//		PuntiVendita pvendita1 = new PuntiVendita("Via Verdi, 18", 1, false);
-		//pvd.save(pvendita1);
-		
-		//pvd.getById("1132b40d-f98a-4ce5-860a-34cacf61385e");
-		
-//		ParcoMezzi mezzoUno = pmd.getMezzo("e4d6336c-5886-4a22-bdd5-40d844066dbc");
-
-		
-//		Biglietto biglietto1 = new Biglietto(LocalDate.of(2023, 05, 22), true, pvendita1, true, mezzoUno, LocalDate.of(2023, 05, 23));
-		//tvd.saveBiglietto(biglietto1);
-		
-//		Biglietto biglietto2 = new Biglietto(LocalDate.of(2023, 04, 27), true, pvendita1, mezzoUno);
-//		tvd.saveBiglietto(biglietto2);
-	
-//		Tessera tesseraUno = td.getById(1004);
-		
-//		Abbonamento abbonamento1 = new Abbonamento(LocalDate.of(2023, 03, 10), true, pvendita1, Periodicità.SETTIMANALE, LocalDate.of(2023, 03, 10).plusDays(7), tesseraUno);
-		//tvd.saveAbbonamento(abbonamento1);
-	
-//		PeriodoManutenzione manutenzione = new PeriodoManutenzione(mezzoUno, LocalDate.of(2022, 12, 01), LocalDate.of(2022, 12, 10));
-		//pmd.savePeriodoManutenzione(manutenzione);
-		
-//		PeriodoServizi servizio = new PeriodoServizi(mezzoUno, LocalDate.of(2022, 12, 10), LocalDate.of(2023, 12, 01));
-		//pmd.savePeriodoServizio(servizio);
-		
-		//System.out.println(mezzoUno.getPeriodoManutenzione());
-		//System.out.println(mezzoUno.getPeriodoServizio());
-		
-		//tvd.checkValiditaAbbonamento(tesseraUno);
-		
-		// ********* METODI DA CONTROLLARE *******
-
-//		pvd.getTicketsBySalesPoint("88db3b5f-e0f3-4341-b933-8d3cdeae2636");
-		
-//		tvd.bigliettiVendutiPerTempo(LocalDate.of(2023, 05, 10), LocalDate.of(2023, 05, 26)).stream()
-//				.forEach(b -> log.info(b.toString()));
-		
-//		mezzoUno.getBiglietti().stream().forEach(b -> log.info(b.toString()));
-		
-//		System.out.println("!!: " + bd.contaBigliettiVidimatiInPeriodo(LocalDate.of(2023, 01, 01), LocalDate.now()));
-		
-//		 ad.getAbbonamentiEmessiInPeriodo(LocalDate.of(2023, 01, 01), LocalDate.now()).stream()
-//			.forEach(b -> log.info(b.toString()));
-		
-//		tvd.vidimazioneBiglietto("8f419c65-65f3-410b-a625-f48167f46c4f");
-		
-		
-		
-		
-		
 		// ********** INTERFACCIA - SWITCH **********
 		
 		Scanner input = new Scanner(System.in);
@@ -167,17 +52,30 @@ public class Application {
 		int chosenAction = -1;
 		
 		log.info("Choose an action: ");
+		System.out.println("1. UTENTI E TESSERE");
+		System.out.println("2. TRATTE E MEZZI");
+		System.out.println("3. PUNTI VENDITA");
+		System.out.println("4. TITOLI DI VIAGGIO");
+		System.out.println("0. ESCI");
 		
 		while(chosenAction != 0) {
 			
 			chosenAction = input.nextInt();
 			
 			switch(chosenAction) {
-			case 1: ;
-			case 2: ;
-			case 3: ;
-			case 4: ;
-			case 5: 
+			case 1: 
+				log.info("UTENI E TESSERE. Scegliere tra: ");
+				gestioneUtentiTessere(input);
+				break;
+			case 2: 
+				log.info("TRATTE E MEZZI. Scegliere tra: ");
+				gestioneMezzi(chosenAction, pmd, input);
+				break;
+			case 3: 
+				log.info("PUNTI VENDITA. Scegliere tra: ");
+				PuntoVendita();
+				break;
+			case 4: 
 				log.info("TITOLI DI VIAGGIO. Scegliere tra: ");
 				System.out.println("1. BIGLIETTI");
 				System.out.println("2. ABBONAMENTI");
@@ -194,7 +92,10 @@ public class Application {
 						System.out.println("Action not available. Please choose a number between 1 and 2 or type 0 to exit the program.");
 				}
 				break;
-			case 0: ;
+			case 0: 
+				input.close();
+				System.out.println("Programma chiuso.");
+				break;
 			default: 
 					System.out.println("Action not available. Please choose a number between 1 and 5 or type 0 to exit the program.");
 					input.close();
@@ -305,9 +206,7 @@ public class Application {
 
 		return LocalDateTime.of(anno, mese, giorno, ora, minuti);
 	}
-	
-	
-
+		
 	public static void PuntoVendita() {
 		Scanner scanner = new Scanner(System.in);
 		
@@ -393,6 +292,7 @@ public class Application {
 		scanner.close();
 
 	}
+	
 	private static void gestioneBiglietti(Scanner input) {
 		
 		System.out.println("1. CREA");
@@ -490,6 +390,93 @@ public class Application {
 		}
 	}
 	
+	private static void gestioneUtentiTessere(Scanner input) {
+		System.out.println("Scegli un opzione:");
+		System.out.println("1. Crea un Utente");
+      System.out.println("2. Crea una Tessera");
+      System.out.println("3. Cerca Utente tramite ID");
+      System.out.println("4. Cerca Tessera tramite ID");
+      System.out.println("5. Cancella Utente tramite ID");
+      System.out.println("6. Cancella Tessera tramite ID");
+      System.out.println("0. Exit");
 
+      int option = input.nextInt();
+      input.nextLine(); // Consume the newline character
+
+      switch (option) {
+          case 1:
+              System.out.println("Inserisci i dettagli per la creazione di un nuovo utente:");
+              System.out.print("Nome: ");
+              String nome = input.nextLine();
+              System.out.print("Cognome: ");
+              String cognome = input.nextLine();
+
+              Utente utente = new Utente(nome, cognome);
+              ud.save(utente);
+              System.out.println("Utente creato con successo.");
+              break;
+
+          case 2:
+              System.out.println("Inserisci i dettagli per la creazione di una nuova tessera:");
+              System.out.print("Data di creazione (yyyy-mm-dd): ");
+              String dataCreazioneStr = input.nextLine();
+              LocalDate dataCreazione = LocalDate.parse(dataCreazioneStr);
+
+              Tessera tessera = new Tessera(dataCreazione);
+              td.save(tessera);
+              System.out.println("tessera creata con successo.");
+              break;
+
+          case 3:
+              System.out.print("Inserisci l'ID Utente: ");
+              long utenteID = input.nextLong();
+              Utente trovaUtente = ud.getById(utenteID);
+              if (trovaUtente != null) {
+                  System.out.println("Utente trovato: " + trovaUtente.toString());
+              } else {
+                  System.out.println("Utente non trovato.");
+              }
+              break;
+
+          case 4:
+              System.out.print("Inserisci l'ID della tua tessera: ");
+              long tesseraId = input.nextLong();
+              Tessera trovaTessera = td.getById(tesseraId);
+              if (trovaTessera != null) {
+                  System.out.println("Tessera Trovata: " + trovaTessera.toString());
+              } else {
+                  System.out.println("Tessera non trovata.");
+              }
+              break;
+
+          case 5:
+              System.out.print("Inserisci l'ID utente per la cancellazione: ");
+              int idUtenteDaCancellare = input.nextInt();
+              input.nextLine();
+              ud.delete(idUtenteDaCancellare);
+              System.out.println("Utente cancellato con successo.");
+              break;
+
+          case 6:
+              System.out.print("Inserisci l'ID della tua tessera per la cancellazione: ");
+              long IdTesseraDaCancellare = input.nextLong();
+              td.delete(IdTesseraDaCancellare);
+              System.out.println("Tessera cancellata con successo.");
+              break;
+
+          case 0:
+              System.out.println("esci...");
+              input.close();
+              System.exit(0);
+              break;
+
+          default:
+              System.out.println("Hai scelto un opzione sbagliata.");
+              break;
+      }
+
+      System.out.println();
+  
+	}
 
 }
